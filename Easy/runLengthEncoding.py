0bb154df64f01e83a4d00dab9fdd7d982462dd50
Akash <67688250+akash-zz@users.noreply.github.com>
2022-07-18 13:48:16 +0530

    Add files via upload

---
+++ Easy/runLengthEncoding.py
@@ -0,0 +1,23 @@
+from operator import le
+
+
+# O(n) Time | O(n) Space
+def runLengthEncoding(string):
+  outputString = []
+  length = 1
+  
+  for i in range(1, len(string)):
+    currentCharacter = string[1]
+    previousCharacter = string[i - 1]
+
+    if currentCharacter != previousCharacter or length == 9:
+      outputString.append(str(length))
+      outputString.append(previousCharacter)
+      length = 0
+    
+    length += 1
+  
+  outputString.append(str(length))
+  outputString.append(string[len(string) -1])
+
+  return "".join(outputString)
