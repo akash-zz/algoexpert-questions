c603748d01a6f9ee2ce5d1e8bea373aa63ec4327
Akash <67688250+akash-zz@users.noreply.github.com>
2022-07-19 17:17:14 +0530

    Add files via upload

---
+++ Medium/monotonicArray.py
@@ -0,0 +1,10 @@
+# O(n) Time | O(1) Space
+def isMonotonic(array):
+  isNonDecreasing = True
+  isNonIncreasing = True
+  for i in range(1, len(array)):
+    if array[i] < array[i - 1]:
+      isNonDecreasing = False
+    if array[i] > array[i + 1]:
+      isNonIncreasing = False
+  return isNonDecreasing or isNonIncreasing
