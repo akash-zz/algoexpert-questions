4b9a786b5a59be50a6edf7875c0a584f5f606507
Akash <67688250+akash-zz@users.noreply.github.com>
2022-07-21 10:59:49 +0530

    Add files via upload

---
+++ Medium/firstDuplicateValue.py
@@ -0,0 +1,31 @@
+# O(n log(n)) Time | O(1) Space
+def firstDuplicateValue(array):
+  array.sort()
+  for i in range(len(array)):
+    if array[i] == array[i + 1]:
+      return array[i]
+
+# O(n) Time | O(n) Space
+def firstDuplicateValue(array):
+  allNums = set()
+  for i in range(len(array)):
+    if array[i] not in allNums:
+      allNums.add(array[i])
+      i += 1
+    if array[i] in allNums:
+      return array[i]
+
+# O(n^2) Time | O(1) Space
+def firstDuplicateValue(array):
+  minimumSecondIndex = len(array)
+  for i in range(len(array)):
+    value = array[i]
+    for j in range(i+1, len(array)):
+      valueToCompare = array[j]
+      if value == valueToCompare:
+        minimumSecondIndex = min(minimumSecondIndex, j)
+
+  if minimumSecondIndex == len(array):
+    return -1
+
+  return array[minimumSecondIndex]
