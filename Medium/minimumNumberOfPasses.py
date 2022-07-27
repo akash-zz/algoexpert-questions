1109fec4c591b512eb75b15f33e641954bd04559
Akash <67688250+akash-zz@users.noreply.github.com>
2022-07-28 01:16:39 +0530

    Add files via upload

---
+++ Medium/minimumNumberOfPasses.py
@@ -0,0 +1,117 @@
+# # O(w * h) Time | O(w * h) Space
+def minimumNumberOfPasses(matrix):
+  passes = convertNegatives(matrix)
+  return passes -1 if not containsNegative(matrix) else -1
+
+def convertNegatives(matrix):
+  nextPassQueue = getAllPositivePositions(matrix)
+  passes = 0
+
+  while len(nextPassQueue) > 0:
+    currentPassQueue = nextPassQueue
+    nextPassQueue = []
+    while len(currentPassQueue) > 0:
+      currentRow, currentCol = currentPassQueue.pop(0)
+
+      adjacentPositions = getAdjacentPositions(currentRow, currentCol, matrix)
+      for position in adjacentPositions:
+        row, col = position
+        value= matrix[row][col]
+        if value < 0:
+          matrix[row][col] = value * -1
+          nextPassQueue.append([row, col])
+
+    passes += 1
+  return passes
+
+def getAllPositivePositions(matrix):
+  positivePositions = []
+
+  for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+      value = matrix[row][col]
+      if value > 0:
+        positivePositions.append([row, col])
+
+  return positivePositions
+
+def getAdjacentPositions(row, col, matrix):
+  adjacentPositions = []
+
+  if row > 0:
+    adjacentPositions.append([row - 1, col])
+  if row < len(matrix) -1:
+    adjacentPositions.append([row + 1, col])
+  if col > 0:
+    adjacentPositions.append([row, col - 1])
+  if col < len(matrix[0]) - 1:
+    adjacentPositions.append([row, col + 1])
+
+  return adjacentPositions
+
+def containsNegative(matrix):
+  for row in matrix:
+    for value in row:
+      if value < 0:
+        return True
+  return False
+
+
+# O(w * h) Time | O(w * h) Space
+def minimumNumberOfPasses(matrix):
+  passes = convertNegatives(matrix)
+  return passes -1 if not containsNegative(matrix) else -1
+
+def convertNegatives(matrix):
+  queue = getAllPositivePositions(matrix)
+  passes = 0
+
+  while len(queue) > 0:
+    currentSize = len(queue)
+    while currentSize > 0:
+      currentRow, currentCol = queue.pop(0)
+
+      adjacentPositions = getAdjacentPositions(currentRow, currentCol, matrix)
+      for position in adjacentPositions:
+        row, col = position
+        value= matrix[row][col]
+        if value < 0:
+          matrix[row][col] = value * -1
+          queue.append([row, col])
+
+      currentSize -= 1
+
+    passes += 1
+  return passes
+
+def getAllPositivePositions(matrix):
+  positivePositions = []
+
+  for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+      value = matrix[row][col]
+      if value > 0:
+        positivePositions.append([row, col])
+
+  return positivePositions
+
+def getAdjacentPositions(row, col, matrix):
+  adjacentPositions = []
+
+  if row > 0:
+    adjacentPositions.append([row - 1, col])
+  if row < len(matrix) -1:
+    adjacentPositions.append([row + 1, col])
+  if col > 0:
+    adjacentPositions.append([row, col - 1])
+  if col < len(matrix[0]) - 1:
+    adjacentPositions.append([row, col + 1])
+
+  return adjacentPositions
+
+def containsNegative(matrix):
+  for row in matrix:
+    for value in row:
+      if value < 0:
+        return True
+  return False
