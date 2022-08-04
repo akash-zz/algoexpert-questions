17f035d9921bde5dcd4abce630b26918c1558634
Akash <67688250+akash-zz@users.noreply.github.com>
2022-08-04 18:20:05 +0530

    Add files via upload

---
+++ Medium/groupAnagrams.py
@@ -0,0 +1,11 @@
+# O(w * n * log(n)) Time | O(wn) Space
+def groupAnagrams(words):
+  anagrams = {}
+  for word in words:
+    sortedWord = "".join(sorted(word))
+    if sortedWord in anagrams:
+      anagrams[sortedWord].append(word)
+    else:
+      anagrams[sortedWord] = [word]
+
+  return list(anagrams.values())
